2b84369d36065ea497d0d3a36cc151c8c83a4fae
unknown <prepaddoo@gmail.com>
2025-03-23 23:17:13 +0100

    First commit

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -311,7 +311,7 @@ export default function ImagePage() {
       formDataForDesign.append("image", compressedFile); // Use the compressed file for AI generation
   
       const imageResponse = await axios.post(
-        `https://a7d4-46-122-65-23.ngrok-free.app/generate_design/?prompt=${translatedPrompt}`,
+        `https://b85d-46-122-65-23.ngrok-free.app/generate_design/?prompt=${translatedPrompt}`,
         formDataForDesign,
         {
           headers: {
